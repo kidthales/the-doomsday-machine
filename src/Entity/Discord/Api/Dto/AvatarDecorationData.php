45b830b1afc7d786da9cfaeff00e2f57590ccb3d
kidthales <kidthales@agogpixel.com>
2024-10-11 20:45:51 -0700

    AvatarDecorationData dto with deserialization test.

---
+++ src/Entity/Discord/Api/Dto/AvatarDecorationData.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Entity\Discord\Api\Dto;
+
+/**
+ * @see https://discord.com/developers/docs/resources/user#avatar-decoration-data-object-avatar-decoration-data-structure
+ */
+class AvatarDecorationData
+{
+    /**
+     * @param string $asset The avatar decoration hash.
+     * @param string $sku_id ID of the avatar decoration's SKU.
+     */
+    public function __construct(public string $asset, public string $sku_id)
+    {
+    }
+}
---
+++ tests/Entity/Discord/Api/Dto/AvatarDecorationDataTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Tests\Entity\Discord\Api\Dto;
+
+use App\Entity\Discord\Api\Dto\AvatarDecorationData;
+use App\Tests\TestHelper\AbstractSerializableSubjectTestCase;
+
+/**
+ * @covers \App\Entity\Discord\Api\Dto\AvatarDecorationData
+ */
+final class AvatarDecorationDataTest extends AbstractSerializableSubjectTestCase
+{
+    /**
+     * @param AvatarDecorationData $expected
+     * @param AvatarDecorationData $actual
+     * @return void
+     */
+    public static function assertDeepSame(mixed $expected, mixed $actual): void
+    {
+        self::assertSame($expected->asset, $actual->asset);
+        self::assertSame($expected->sku_id, $actual->sku_id);
+    }
+
+    /**
+     * @return array
+     */
+    public static function provider_deserialization(): array
+    {
+        return [
+            [
+                '{"asset":"test-asset","sku_id":"test-sku-id"}',
+                new AvatarDecorationData('test-asset', 'test-sku-id')
+            ]
+        ];
+    }
+
+    /**
+     * @param string $subject
+     * @param AvatarDecorationData $expected
+     * @return void
+     * @dataProvider provider_deserialization
+     */
+    public function test_deserialization(string $subject, AvatarDecorationData $expected): void
+    {
+        self::testDeserialization($subject, $expected, AvatarDecorationData::class);
+    }
+}
