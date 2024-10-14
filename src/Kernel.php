fed5bf5f906a50b145861f9826ec90b786c40aa6
kidthales <kidthales@agogpixel.com>
2024-10-14 12:43:26 -0700

    Get Current Application Command (#14)
    * Wire up ConsoleStyleDefinitionListConverterPass with kernel.
    
    * GetCurrentApplicationCommand with test coverage.

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,7 +2,9 @@
 
 namespace App;
 
+use App\DependencyInjection\Compiler\ConsoleStyleDefinitionListConverterPass;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 /**
@@ -11,4 +13,13 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    /**
+     * @param ContainerBuilder $container
+     * @return void
+     */
+    protected function build(ContainerBuilder $container): void
+    {
+        $container->addCompilerPass(new ConsoleStyleDefinitionListConverterPass());
+    }
 }
---
+++ tests/Command/Discord/GetCurrentApplicationCommandTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Tests\Command\Discord;
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * @covers \App\Command\Discord\GetCurrentApplicationCommand
+ */
+final class GetCurrentApplicationCommandTest extends KernelTestCase
+{
+    /**
+     * @return void
+     */
+    public function test_execute(): void
+    {
+        self::bootKernel();
+
+        $application = new Application(self::$kernel);
+
+        $command = $application->find('app:discord:get-current-application');
+
+        $commandTester = new CommandTester($command);
+
+        $commandTester->execute([]);
+
+        $commandTester->assertCommandIsSuccessful();
+
+        $actual = $commandTester->getDisplay();
+
+        self::assertStringContainsString('[DEV] The Doomsday Machine', $actual);
+    }
+}
