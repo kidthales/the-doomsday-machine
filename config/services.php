7dc079789ea1f23ca05d8bed2182bc619ada8c0d
kidthales <kidthales@agogpixel.com>
2026-01-06 22:54:42 -0800

    File Depot (#5)
    * Ensure file_depot directory is created in entrypoint.
    
    * make composer c='req symfony/filesystem'
    
    * Define a FileDepot service with a makePath method.

--- config/services.php
+++ config/services.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 return static function (ContainerConfigurator $container): void {
+    $container->parameters()
+        ->set('app.file_depot_path', '%kernel.project_dir%/data/%kernel.environment%/file_depot');
+
     $services = $container->services()->defaults()
         ->autowire()
         ->autoconfigure();
