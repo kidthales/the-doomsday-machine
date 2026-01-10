d759b62bcc2f8192d2cc6cbcf94594768f53e3a3
kidthales <kidthales@agogpixel.com>
2026-01-10 02:07:38 -0800

    (WIP) scrapeAvailableSeasons method at full test coverage.

--- tests/FootyStats/ScraperTest.php
+++ tests/FootyStats/ScraperTest.php
@@ -78,8 +78,98 @@ final class ScraperTest extends KernelTestCase
             ],
             'Empty Content' => [
                 ['England', 'Championship'],
-                [new MockResponse(''), new MockResponse(''), new MockResponse('')],
+                [new MockResponse(''), new MockResponse('')],
                 ['current' => '', 'previous' => ['overview' => [], 'fixtures' => []]]
+            ],
+            'Mismatched Content' => [
+                ['England', 'Championship'],
+                [
+                    new MockResponse(<<<'HTML'
+<div id="teamSummary">
+    <div class="season">
+        <div class="drop-down-parent">
+            2025/26
+            <ul class="drop-down">
+                <li><a href="#" class="changeLeagueDataButton" data-z="12451" data-hash="20242025" data-zzzz="form-table" data-zzz="overview">2024/25</a></li>
+                <li><a href="#" class="changeLeagueDataButton" data-z="9663" data-hash="20232024" data-zzzz="form-table" data-zzz="overview">2023/24</a></li>
+                <li><a href="#" class="changeLeagueDataButton" data-z="7593" data-hash="20222023" data-zzzz="form-table" data-zzz="overview">2022/23</a></li>
+                <li><a href="#" class="changeLeagueDataButton" data-z="6089" data-hash="20212022" data-zzzz="form-table" data-zzz="overview">2021/22</a></li>
+                <li><a href="#" class="changeLeagueDataButton" data-z="4912" data-hash="20202021" data-zzzz="form-table" data-zzz="overview">2020/21</a></li>
+                <li><a href="#" class="changeLeagueDataButton" data-z="2187" data-hash="20192020" data-zzzz="form-table" data-zzz="overview">2019/20</a></li>
+                <li><a href="#" class="changeLeagueDataButton" data-z="1624" data-hash="20182019" data-zzzz="form-table" data-zzz="overview">2018/19</a></li>
+                <li><a href="#" class="changeLeagueDataButton" data-z="165" data-hash="20172018" data-zzzz="form-table" data-zzz="overview">2017/18</a></li>
+                <li><a href="#" class="changeLeagueDataButton" data-z="22" data-hash="20162017" data-zzzz="form-table" data-zzz="overview">2016/17</a></li>
+                <li><a href="#" class="changeLeagueDataButton" data-z="25" data-hash="20152016" data-zzzz="form-table" data-zzz="overview">2015/16</a></li>
+                <li><a href="#" class="changeLeagueDataButton" data-z="26" data-hash="20142015" data-zzzz="form-table" data-zzz="overview">2014/15</a></li>
+                <li><a href="#" class="changeLeagueDataButton" data-z="27" data-hash="20132014" data-zzzz="form-table" data-zzz="overview">2013/14</a></li>
+                <li><a href="#" class="changeLeagueDataButton" data-z="3141" data-hash="20122013" data-zzzz="form-table" data-zzz="overview">2012/13</a></li>
+                <li><a href="#" class="changeLeagueDataButton" data-z="3143" data-hash="20112012" data-zzzz="form-table" data-zzz="overview">2011/12</a></li>
+                <li><a href="#" class="changeLeagueDataButton" data-z="3146" data-hash="20102011" data-zzzz="form-table" data-zzz="overview">2010/11</a></li>
+                <li><a href="#" class="changeLeagueDataButton" data-z="3147" data-hash="20092010" data-zzzz="form-table" data-zzz="overview">2009/10</a></li>
+                <li><a href="#" class="changeLeagueDataButton" data-z="8031" data-hash="20082009" data-zzzz="form-table" data-zzz="overview">2008/09</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+HTML),
+                    new MockResponse('')
+                ],
+                new RuntimeException('Overview current season (2025/26) does not match fixtures current season ()')
+            ],
+            'Mismatched Content 2' => [
+                ['England', 'Championship'],
+                [
+                    new MockResponse(<<<'HTML'
+<div id="teamSummary">
+    <div class="season">
+        <div class="drop-down-parent">
+            2025/26
+            <ul class="drop-down">
+                <li><a href="#" class="changeLeagueDataButton" data-z="12451" data-hash="20242025" data-zzzz="form-table" data-zzz="overview">2024/25</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+HTML),
+                    new MockResponse(<<<'HTML'
+<div id="teamSummary">
+    <div class="season">
+        <div class="drop-down-parent">
+            2025/26
+        </div>
+    </div>
+</div>
+HTML)
+                ],
+                new RuntimeException('Overview previous seasons count (1) does not match fixtures previous seasons count (0)')
+            ],
+            'Mismatched Content 3' => [
+                ['England', 'Championship'],
+                [
+                    new MockResponse(<<<'HTML'
+<div id="teamSummary">
+    <div class="season">
+        <div class="drop-down-parent">
+            2025/26
+            <ul class="drop-down">
+                <li><a href="#" class="changeLeagueDataButton" data-z="12451" data-hash="20242025" data-zzzz="form-table" data-zzz="overview">2024/25</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+HTML),
+                    new MockResponse(<<<'HTML'
+<div id="teamSummary">
+    <div class="season">
+        <div class="drop-down-parent">
+            2025/26
+            <li><a href="#" class="changeLeagueDataButton" data-z="8031" data-hash="20082009" data-zzzz="form-table" data-zzz="fixtures">2008/09</a></li>
+        </div>
+    </div>
+</div>
+HTML)
+                ],
+                new RuntimeException('Overview previous season (2024/25) does not match fixtures previous season (2008/09)')
             ]
         ];
     }
