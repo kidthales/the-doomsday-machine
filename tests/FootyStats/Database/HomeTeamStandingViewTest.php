f4e94fac55a1f7a291bcf838cda8e5d861421191
kidthales <kidthales@agogpixel.com>
2026-01-11 00:46:09 -0800

    Define team standing views with test coverage.

--- tests/FootyStats/Database/HomeTeamStandingViewTest.php
+++ tests/FootyStats/Database/HomeTeamStandingViewTest.php
@@ -4,22 +4,22 @@ declare(strict_types=1);
 
 namespace App\Tests\FootyStats\Database;
 
-use App\FootyStats\Database\MatchTable;
+use App\FootyStats\Database\HomeTeamStandingView;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
-final class MatchTableTest extends TestCase
+final class HomeTeamStandingViewTest extends TestCase
 {
     public static function provide_test_getName(): array
     {
         return [
             'England Championship 2025/26' => [
                 ['England', 'Championship', '2025/26'],
-                'england_championship_202526_match'
+                'england_championship_202526_home_team_standing'
             ],
             'England Premier League 2012/13' => [
                 ['England', 'Premier League', '2012/13'],
-                'england_premier_league_201213_match'
+                'england_premier_league_201213_home_team_standing'
             ],
         ];
     }
@@ -27,7 +27,7 @@ final class MatchTableTest extends TestCase
     #[DataProvider('provide_test_getName')]
     public function test_getName(array $subject, string $expected): void
     {
-        $actual = MatchTable::getName(...$subject);
+        $actual = HomeTeamStandingView::getName(...$subject);
         self::assertSame($expected, $actual);
     }
 
@@ -36,11 +36,11 @@ final class MatchTableTest extends TestCase
         return [
             'England Championship 2025/26' => [
                 ['England', 'Championship', '2025/26'],
-                'DROP TABLE england_championship_202526_match;'
+                'DROP VIEW england_championship_202526_home_team_standing;'
             ],
             'England Premier League 2012/13' => [
                 ['England', 'Premier League', '2012/13'],
-                'DROP TABLE england_premier_league_201213_match;'
+                'DROP VIEW england_premier_league_201213_home_team_standing;'
             ],
         ];
     }
@@ -48,7 +48,7 @@ final class MatchTableTest extends TestCase
     #[DataProvider('provide_test_getDropSql')]
     public function test_getDropSql(array $subject, string $expected): void
     {
-        $actual = MatchTable::getDropSql(...$subject);
+        $actual = HomeTeamStandingView::getDropSql(...$subject);
         self::assertSame($expected, $actual);
     }
 
@@ -57,11 +57,11 @@ final class MatchTableTest extends TestCase
         return [
             'England Championship 2025/26' => [
                 ['England', 'Championship', '2025/26'],
-                'CREATE TABLE england_championship_202526_match ('
+                'CREATE VIEW england_championship_202526_home_team_standing AS'
             ],
             'England Premier League 2012/13' => [
                 ['England', 'Premier League', '2012/13'],
-                'CREATE TABLE england_premier_league_201213_match ('
+                'CREATE VIEW england_premier_league_201213_home_team_standing AS'
             ],
         ];
     }
@@ -69,7 +69,7 @@ final class MatchTableTest extends TestCase
     #[DataProvider('provide_test_getCreateSql')]
     public function test_getCreateSql(array $subject, string $expected): void
     {
-        $actual = MatchTable::getCreateSql(...$subject);
+        $actual = HomeTeamStandingView::getCreateSql(...$subject);
         self::assertStringContainsString($expected, $actual);
     }
 }
