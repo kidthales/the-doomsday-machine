b3c6d97f58161732120a8a63dd720160f8db01fd
kidthales <kidthales@agogpixel.com>
2026-05-17 19:35:10 -0700

    Basic integration test implementations for football organization crud commands.

---
+++ tests/Integration/Command/Jabronibetz/Football/Organization/CreateCommandTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Integration\Command\Jabronibetz\Football\Organization;
+
+use App\Command\Jabronibetz\Football\Organization\CreateCommand;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\Test;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Tester\ApplicationTester;
+
+/**
+ * @author Tristan Bonsor <kidthales@agogpixel.com>
+ */
+#[Group('jabronibetz')]
+#[CoversClass(CreateCommand::class)]
+final class CreateCommandTest extends KernelTestCase
+{
+    #[Test]
+    public function it_creates_football_organization_non_interactively(): void
+    {
+        $this->bootKernel();
+
+        $app = new Application(self::$kernel);
+        $app->setAutoExit(false);
+
+        $appTester = new ApplicationTester($app);
+        $appTester->run(
+            [
+                'command' => 'app:jabronibetz:football:organization:create',
+                'name' => 'Test Association',
+                'acronym' => 'TA',
+            ],
+            [
+                'interactive' => false
+            ]
+        );
+
+        $appTester->assertCommandIsSuccessful('Football organization Test Association (TA) has been created with id 1.');
+    }
+}
