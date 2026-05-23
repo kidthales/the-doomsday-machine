3a4d7954ca30f939311ed349afb6fc26c2eab239
kidthales <kidthales@agogpixel.com>
2026-05-22 18:12:28 -0700

    Implement bfrpg rules source read command with basic test support.

---
+++ src/Command/BFRPG/Rules/Source/ReadCommand.php
@@ -0,0 +1,133 @@
+<?php
+/*
+ * The Doomsday Machine
+ * Copyright (C) 2026  Tristan Bonsor
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace App\Command\BFRPG\Rules\Source;
+
+use App\Domain\BFRPG\Entity\RulesSource;
+use App\Domain\BFRPG\Repository\RulesSourceRepository;
+use App\Domain\Shared\Console\Style\DefinitionListConverter;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Throwable;
+
+/**
+ * @author Tristan Bonsor <kidthales@agogpixel.com>
+ */
+#[AsCommand(
+    name: 'app:bfrpg:rules:source:read',
+    description: 'Read a rules source',
+    aliases: ['app:bfrpg:rules:src:read'],
+)]
+final class ReadCommand extends Command
+{
+    /**
+     * @param RulesSourceRepository $rulesSourceRepository
+     * @param DefinitionListConverter $definitionListConverter
+     */
+    public function __construct(
+        private readonly RulesSourceRepository   $rulesSourceRepository,
+        private readonly DefinitionListConverter $definitionListConverter
+    )
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @return void
+     */
+    protected function configure(): void
+    {
+        $this
+            ->addArgument(
+                name: 'id',
+                mode: InputArgument::REQUIRED,
+                description: 'The id of the rules source'
+            )
+            ->setHelp(
+                <<<'HELP'
+                The <info>%command.name%</info> command allows you to read a <comment>rules source</comment>
+                in the <comment>BFRPG</comment> db.
+
+                Usage:
+                  <info>%command.full_name% <id></info>
+
+                Examples:
+                  <info>%command.full_name% 1</info>
+
+                If no id is specified, you'll be prompted interactively.
+                HELP
+            );
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return void
+     */
+    protected function interact(InputInterface $input, OutputInterface $output): void
+    {
+        /** @var QuestionHelper $helper */
+        $helper = $this->getHelper('question');
+
+        if ($input->getArgument('id') === null) {
+            $input->setArgument('id', $helper->ask($input, $output, new Question('Rules source id: ')));
+        }
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $io->title('BFRPG: Rules Source Read');
+
+        try {
+            $source = $this->rulesSourceRepository->find($input->getArgument('id'));
+
+            if ($source === null) {
+                $io->error('Rules source not found');
+                return Command::FAILURE;
+            }
+
+            $io->definitionList(...$this->definitionListConverter->convert(
+                $source,
+                [
+                    AbstractNormalizer::GROUPS => RulesSource::GROUP_READ
+                ]
+            ));
+        } catch (Throwable $e) {
+            $io->error($e->getMessage());
+            return Command::FAILURE;
+        }
+
+        return Command::SUCCESS;
+    }
+}
