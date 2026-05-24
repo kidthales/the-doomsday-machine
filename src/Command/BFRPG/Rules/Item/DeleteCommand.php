7cd88cc86cbfa1d661cf5988cd70a70a512e4920
kidthales <kidthales@agogpixel.com>
2026-05-23 22:40:59 -0700

    Implement bfrpg rules item delete command with minimal test coverage.

---
+++ src/Command/BFRPG/Rules/Item/DeleteCommand.php
@@ -0,0 +1,145 @@
+<?php
+/*
+ * The Doomsday Machine
+ * Copyright (C) 2026  Tristan Bonsor
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace App\Command\BFRPG\Rules\Item;
+
+use App\Domain\BFRPG\Entity\RulesItem;
+use App\Domain\Shared\Console\Style\DefinitionListConverter;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Throwable;
+
+/**
+ * @author Tristan Bonsor <kidthales@agogpixel.com>
+ */
+#[AsCommand(
+    name: 'app:bfrpg:rules:item:delete',
+    description: 'Delete a rules item',
+    aliases: ['app:bf:rls:itm:delete'],
+)]
+final class DeleteCommand extends Command
+{
+    /**
+     * @param EntityManagerInterface $bfrpgEntityManager Autowiring alias
+     */
+    public function __construct(
+        private readonly EntityManagerInterface  $bfrpgEntityManager,
+        private readonly DefinitionListConverter $definitionListConverter,
+    )
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @return void
+     */
+    protected function configure(): void
+    {
+        $this
+            ->addArgument(
+                name: 'id',
+                mode: InputArgument::REQUIRED,
+                description: 'The id of the rules item'
+            )
+            ->setHelp(
+                <<<'HELP'
+                The <info>%command.name%</info> command allows you to delete a <comment>rules item</comment>
+                in the <comment>BFRPG</comment> db.
+
+                Usage:
+                  <info>%command.full_name% <id></info>
+
+                Examples:
+                  <info>%command.full_name% 1</info>
+
+                If no id is specified, you'll be prompted interactively.
+                HELP
+            );
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return void
+     */
+    protected function interact(InputInterface $input, OutputInterface $output): void
+    {
+        /** @var QuestionHelper $helper */
+        $helper = $this->getHelper('question');
+
+        if ($input->getArgument('id') === null) {
+            $input->setArgument('id', $helper->ask($input, $output, new Question('Rules item id: ')));
+        }
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $io->title('BFRPG: Rules Item Delete');
+
+        try {
+            $item = $this->bfrpgEntityManager->find(RulesItem::class, $input->getArgument('id'));
+
+            if ($item === null) {
+                $io->error('Rules item not found');
+                return Command::FAILURE;
+            }
+
+            if ($input->isInteractive()) {
+                $io->definitionList(...$this->definitionListConverter->convert(
+                    $item,
+                    [
+                        AbstractNormalizer::GROUPS => RulesItem::GROUP_DETAIL
+                    ]
+                ));
+
+                if (!$io->confirm('Delete rules item?')) {
+                    return Command::SUCCESS;
+                }
+            }
+
+            $id = $item->getId();
+
+            $this->bfrpgEntityManager->remove($item);
+            $this->bfrpgEntityManager->flush();
+
+            $io->success(sprintf('Rules item %s with id %d has been deleted.', $item->getName(), $id));
+        } catch (Throwable $e) {
+            $io->error($e->getMessage());
+            return Command::FAILURE;
+        }
+
+        return Command::SUCCESS;
+    }
+}
---
+++ tests/Integration/Command/BFRPG/Rules/Item/DeleteCommandTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Integration\Command\BFRPG\Rules\Item;
+
+use App\Command\BFRPG\Rules\Item\DeleteCommand;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\Test;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Tester\ApplicationTester;
+
+/**
+ * @author Tristan Bonsor <kidthales@agogpixel.com>
+ */
+#[Group('bfrpg')]
+#[CoversClass(DeleteCommand::class)]
+final class DeleteCommandTest extends KernelTestCase
+{
+    #[Test]
+    public function it_fails_when_rules_item_id_not_found(): void
+    {
+        $this->bootKernel();
+
+        $app = new Application(self::$kernel);
+        $app->setAutoExit(false);
+
+        $appTester = new ApplicationTester($app);
+        $appTester->run(
+            [
+                'command' => 'app:bfrpg:rules:item:delete',
+                'id' => -1,
+            ]
+        );
+
+        $this->assertSame(1, $appTester->getStatusCode());
+        $this->assertStringContainsString('Rules item not found', $appTester->getDisplay());
+    }
+}
