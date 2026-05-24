9d14ac0ada4c20d76f74701aa768087930f46c5d
kidthales <kidthales@agogpixel.com>
2026-05-24 16:57:58 -0700

    Implement delete command with minimal coverage.

---
+++ src/Command/Jabronibetz/Football/CompetitionTeamEntry/DeleteCommand.php
@@ -0,0 +1,162 @@
+<?php
+/*
+ * The Doomsday Machine
+ * Copyright (C) 2026  Tristan Bonsor
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace App\Command\Jabronibetz\Football\CompetitionTeamEntry;
+
+use App\Domain\Jabronibetz\Entity\FootballCompetition;
+use App\Domain\Jabronibetz\Entity\FootballCompetitionTeamEntry;
+use App\Domain\Jabronibetz\Entity\FootballTeam;
+use App\Domain\Jabronibetz\Repository\FootballCompetitionTeamEntryRepository;
+use App\Domain\Shared\Console\Style\DefinitionListConverter;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Throwable;
+
+/**
+ * @author Tristan Bonsor <kidthales@agogpixel.com>
+ */
+#[AsCommand(
+    name: 'app:jabronibetz:football:competition-team-entry:delete',
+    description: 'Delete a football competition team entry',
+    aliases: ['app:jbetz:footy:cmp-team-entry:delete'],
+)]
+final class DeleteCommand extends Command
+{
+    /**
+     * @param EntityManagerInterface $jabronibetzEntityManager Autowiring alias
+     */
+    public function __construct(
+        private readonly EntityManagerInterface  $jabronibetzEntityManager,
+        private readonly DefinitionListConverter $definitionListConverter
+    )
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @return void
+     */
+    protected function configure(): void
+    {
+        $this
+            ->addArgument(
+                name: 'id',
+                mode: InputArgument::REQUIRED,
+                description: 'The id of the football competition team entry'
+            )
+            ->setHelp(
+                <<<'HELP'
+                The <info>%command.name%</info> command allows you to delete a
+                <comment>football competition team entry</comment> in the <comment>Jabronibetz</comment> db.
+
+                Usage:
+                  <info>%command.full_name% <id></info>
+
+                Examples:
+                  <info>%command.full_name% 1</info>
+
+                If no id is specified, you'll be prompted interactively.
+                HELP
+            );
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return void
+     */
+    protected function interact(InputInterface $input, OutputInterface $output): void
+    {
+        /** @var QuestionHelper $helper */
+        $helper = $this->getHelper('question');
+
+        if ($input->getArgument('id') === null) {
+            /** @var FootballCompetitionTeamEntryRepository $repo */
+            $repo = $this->jabronibetzEntityManager->getRepository(FootballCompetitionTeamEntry::class);
+            $choices = $repo->findAllChoices();
+
+            if (!empty($choices)) {
+                $choice = $helper->ask($input, $output, new ChoiceQuestion('Football competition team entry id: ', $choices));
+                $input->setArgument('id', array_search($choice, $choices, true));
+            }
+        }
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $io->title('Jabronibetz: Football Competition Team Entry Delete');
+
+        try {
+            $entry = $this->jabronibetzEntityManager->find(FootballCompetitionTeamEntry::class, $input->getArgument('id'));
+            if ($entry === null) {
+                $io->error('Football competition team entry not found');
+                return Command::FAILURE;
+            }
+
+            if ($input->isInteractive()) {
+                $io->definitionList(...$this->definitionListConverter->convert(
+                    $entry,
+                    [
+                        AbstractNormalizer::GROUPS => [
+                            FootballCompetitionTeamEntry::GROUP_DETAIL,
+                            FootballCompetition::GROUP_LIST,
+                            FootballTeam::GROUP_LIST
+                        ]
+                    ]
+                ));
+
+                if (!$io->confirm('Delete football competition team entry?')) {
+                    return Command::SUCCESS;
+                }
+            }
+
+            $id = $entry->getId();
+
+            $this->jabronibetzEntityManager->remove($entry);
+            $this->jabronibetzEntityManager->flush();
+
+            $io->success(sprintf(
+                'Football competition team entry %s with id %d has been deleted.',
+                $entry->getChoiceValue(),
+                $id
+            ));
+        } catch (Throwable $e) {
+            $io->error($e->getMessage());
+            return Command::FAILURE;
+        }
+
+        return Command::SUCCESS;
+    }
+}
