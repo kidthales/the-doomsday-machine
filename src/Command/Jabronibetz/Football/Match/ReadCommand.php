4e2635adbdb9d6825125b46448d4affadc50d63d
kidthales <kidthales@agogpixel.com>
2026-05-26 18:55:23 -0700

    Implement football match read command with minimal test coverage.

---
+++ src/Command/Jabronibetz/Football/Match/ReadCommand.php
@@ -0,0 +1,143 @@
+<?php
+/*
+ * The Doomsday Machine
+ * Copyright (C) 2026  Tristan Bonsor
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace App\Command\Jabronibetz\Football\Match;
+
+use App\Domain\Jabronibetz\Entity\FootballCompetition;
+use App\Domain\Jabronibetz\Entity\FootballMatch;
+use App\Domain\Jabronibetz\Entity\FootballTeam;
+use App\Domain\Jabronibetz\Repository\FootballMatchRepository;
+use App\Domain\Shared\Console\Style\DefinitionListConverter;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Throwable;
+
+/**
+ * @author Tristan Bonsor <kidthales@agogpixel.com>
+ */
+#[AsCommand(
+    name: 'app:jabronibetz:football:match:read',
+    description: 'Read a football match',
+    aliases: ['app:jbetz:footy:match:read'],
+)]
+final class ReadCommand extends Command
+{
+    /**
+     * @param FootballMatchRepository $footballMatchRepository
+     * @param DefinitionListConverter $definitionListConverter
+     */
+    public function __construct(
+        private readonly FootballMatchRepository $footballMatchRepository,
+        private readonly DefinitionListConverter $definitionListConverter
+    )
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @return void
+     */
+    protected function configure(): void
+    {
+        $this
+            ->addArgument(
+                name: 'id',
+                mode: InputArgument::REQUIRED,
+                description: 'The id of the football match'
+            )
+            ->setHelp(
+                <<<'HELP'
+                The <info>%command.name%</info> command allows you to read a
+                <comment>football match</comment> in the <comment>Jabronibetz</comment> db.
+
+                Usage:
+                  <info>%command.full_name% <id></info>
+
+                Examples:
+                  <info>%command.full_name% 1</info>
+
+                If no id is specified, you'll be prompted interactively.
+                HELP
+            );
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return void
+     */
+    protected function interact(InputInterface $input, OutputInterface $output): void
+    {
+        /** @var QuestionHelper $helper */
+        $helper = $this->getHelper('question');
+
+        if ($input->getArgument('id') === null) {
+            $choices = $this->footballMatchRepository->findAllChoices();
+
+            if (!empty($choices)) {
+                $choice = $helper->ask($input, $output, new ChoiceQuestion('Football match id: ', $choices));
+                $input->setArgument('id', array_search($choice, $choices, true));
+            }
+        }
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $io->title('Jabronibetz: Football Match Read');
+
+        try {
+            $match = $this->footballMatchRepository->find($input->getArgument('id'));
+            if ($match === null) {
+                $io->error('Football match not found');
+                return Command::FAILURE;
+            }
+
+            $io->definitionList(...$this->definitionListConverter->convert(
+                $match,
+                [
+                    AbstractNormalizer::GROUPS => [
+                        FootballMatch::GROUP_DETAIL,
+                        FootballCompetition::GROUP_LIST,
+                        FootballTeam::GROUP_LIST
+                    ]
+                ]
+            ));
+        } catch (Throwable $e) {
+            $io->error($e->getMessage());
+            return Command::FAILURE;
+        }
+
+        return Command::SUCCESS;
+    }
+}
