65bd42a43a3e4f617d1ef2012ab8d694b11d4029
kidthales <kidthales@agogpixel.com>
2026-05-27 21:47:16 -0700

    Reduce namespace depth on jbetz football entity commands.

---
+++ tests/Integration/Command/Jabronibetz/Entity/FootballMatch/ListCommandTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Integration\Command\Jabronibetz\Entity\FootballMatch;
+
+use App\Command\Jabronibetz\Entity\FootballMatch\ListCommand;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\Test;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Tester\ApplicationTester;
+
+/**
+ * @author Tristan Bonsor <kidthales@agogpixel.com>
+ */
+#[Group('jabronibetz')]
+#[CoversClass(ListCommand::class)]
+final class ListCommandTest extends KernelTestCase
+{
+    #[Test]
+    public function it_displays_a_count_of_football_matches_found(): void
+    {
+        $this->bootKernel();
+
+        $app = new Application(self::$kernel);
+        $app->setAutoExit(false);
+
+        $appTester = new ApplicationTester($app);
+        $appTester->run(
+            [
+                'command' => 'app:jabronibetz:entity:football-match:list',
+            ]
+        );
+
+        $appTester->assertCommandIsSuccessful();
+
+        $this->assertStringContainsString('Found 0 football matches.', $appTester->getDisplay());
+    }
+}
