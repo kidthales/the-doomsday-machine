e4def872dd7112f213013a0ba8501c75c0b35f4f
kidthales <kidthales@agogpixel.com>
2026-06-28 01:25:04 -0700

    (WIP) Stub the discord message eraser bot.

---
+++ src/Domain/Discord/MessageEraserBot/MessageEraserBot.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * The Doomsday Machine
+ * Copyright (C) 2026  Tristan Bonsor
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace App\Domain\Discord\MessageEraserBot;
+
+use App\Domain\Shared\Discord\DiscordBot;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+
+/**
+ * @author Tristan Bonsor <kidthales@agogpixel.com>
+ */
+final class MessageEraserBot extends DiscordBot
+{
+    /**
+     * @param string $token
+     */
+    public function __construct(#[Autowire(env: 'DISCORD_MESSAGE_ERASER_BOT_TOKEN')] string $token)
+    {
+        parent::__construct($token);
+    }
+
+    // TODO
+}
---
+++ src/Domain/Discord/MessageEraserBot/MessageEraserBotAwareTrait.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * The Doomsday Machine
+ * Copyright (C) 2026  Tristan Bonsor
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace App\Domain\Discord\MessageEraserBot;
+
+use Symfony\Contracts\Service\Attribute\Required;
+
+/**
+ * @author Tristan Bonsor <kidthales@agogpixel.com>
+ */
+trait MessageEraserBotAwareTrait
+{
+    /**
+     * @var MessageEraserBot|null
+     */
+    protected ?MessageEraserBot $messageEraserBot = null;
+
+    /**
+     * @return MessageEraserBot|null
+     */
+    public function getMessageEraserBot(): ?MessageEraserBot
+    {
+        return $this->messageEraserBot;
+    }
+
+    /**
+     * @param MessageEraserBot $provider
+     * @return $this
+     */
+    #[Required]
+    public function setMessageEraserBot(MessageEraserBot $provider): static
+    {
+        $this->messageEraserBot = $provider;
+        return $this;
+    }
+}
