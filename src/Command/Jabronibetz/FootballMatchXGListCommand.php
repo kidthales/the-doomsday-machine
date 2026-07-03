b3e1105f075fe4969befd02d161626b8e2d84212
kidthales <kidthales@agogpixel.com>
2026-07-03 12:26:53 -0700

    (WIP) Refactor choice handling and minor clean up in remaining football commands.

--- src/Command/Jabronibetz/FootballMatchXGListCommand.php
+++ src/Command/Jabronibetz/FootballMatchXGListCommand.php
@@ -21,13 +21,10 @@ declare(strict_types=1);
 
 namespace App\Command\Jabronibetz;
 
-use App\Domain\Jabronibetz\Calculator\FootballCalculatorAwareTrait;
+use App\Domain\Jabronibetz\Console\Command\Command;
 use App\Domain\Jabronibetz\DataProvider\FootballCompetitionDataProviderAwareTrait;
 use App\Domain\Jabronibetz\Entity\FootballCompetition;
 use App\Domain\Jabronibetz\Entity\FootballMatch;
-use App\Domain\Jabronibetz\ORM\EntityManagerAwareTrait;
-use App\Domain\Shared\Console\Command\Command;
-use App\Domain\Shared\Console\Style\DefinitionListConverterAwareTrait;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
@@ -46,9 +43,7 @@ use Throwable;
 )]
 final class FootballMatchXGListCommand extends Command
 {
-    use DefinitionListConverterAwareTrait,
-        EntityManagerAwareTrait,
-        FootballCompetitionDataProviderAwareTrait;
+    use FootballCompetitionDataProviderAwareTrait;
 
     private const array HEADERS = [
         'Match',
@@ -105,14 +100,7 @@ final class FootballMatchXGListCommand extends Command
      */
     protected function interact(InputInterface $input, OutputInterface $output): void
     {
-        $this->interactChoiceQuestionWithChoosables(
-            $input,
-            $output,
-            'competition-id',
-            'Football competition id: ',
-            $this->entityManager->getRepository(FootballCompetition::class)->findAll(),
-            true
-        );
+        $this->interactFootballCompetition($input, $output, 'competition-id', 'Football competition: ');
     }
 
     /**
@@ -127,7 +115,7 @@ final class FootballMatchXGListCommand extends Command
         try {
             $competition = $this->entityManager->find(FootballCompetition::class, $input->getArgument('competition-id'));
             if ($competition === null) {
-                $io->error('Football competition not found');
+                $io->error('Football competition not found.');
                 return Command::FAILURE;
             }
 
@@ -137,7 +125,7 @@ final class FootballMatchXGListCommand extends Command
             $limit = $input->getOption('limit');
             if ($limit !== null) {
                 if (!is_numeric($limit)) {
-                    $io->error('Limit quantity must be a numeric value');
+                    $io->error('Limit quantity must be a numeric value.');
                     return Command::FAILURE;
                 }
                 $limit = intval($limit);
